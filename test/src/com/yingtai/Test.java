920f1d77894910c378d6c453b48df3b1d751365f
wuxunhuan <wu1781342424@163.com>
2021-01-22 16:49:01 +0800

    第一次测试

---
+++ test/src/com/yingtai/Test.java
@@ -0,0 +1,11 @@
+package com.yingtai;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("第一次提交");
+    }
+}
