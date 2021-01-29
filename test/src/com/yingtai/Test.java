503dd895fae1aa0ff26572ca4dfc2cdc7e6809a0
wuxunhuan <wu1781342424@163.com>
2021-01-29 13:36:36 +0800

    远程回退

--- test/src/com/yingtai/Test.java
+++ test/src/com/yingtai/Test.java
@@ -7,8 +7,5 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("第一次提交");
-        System.out.println("第二次提交");
-        System.out.println("第三次提交");
-
     }
 }
