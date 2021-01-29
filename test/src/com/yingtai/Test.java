245432c7d05c30f89c40de016927aa3a262d6940
wuxunhuan <wu1781342424@163.com>
2021-01-29 13:52:48 +0800

    分支

--- test/src/com/yingtai/Test.java
+++ test/src/com/yingtai/Test.java
@@ -6,8 +6,8 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("第一次提交");
-        System.out.println("第二次提交");
+        System.out.println("333");
+        System.out.println("666");
         System.out.println("666");
     }
 }
